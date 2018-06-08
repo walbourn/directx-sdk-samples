488e56893a7b18414cb9596151305ac91930409c
Chuck Walbourn <chuckw@windows.microsoft.com>
2018-06-07 18:24:45 -0700

    Minor code review feedback

--- XAudio2/Common/WaveBankReader.cpp
+++ XAudio2/Common/WaveBankReader.cpp
@@ -30,7 +30,7 @@ struct handle_closer { void operator()(HANDLE h) { if (h) CloseHandle(h); } };
 
 typedef public std::unique_ptr<void, handle_closer> ScopedHandle;
 
-inline HANDLE safe_handle( HANDLE h ) { return (h == INVALID_HANDLE_VALUE) ? 0 : h; }
+inline HANDLE safe_handle( HANDLE h ) { return (h == INVALID_HANDLE_VALUE) ? nullptr : h; }
 
 //--------------------------------------------------------------------------------------
 #pragma pack(push, 1)
